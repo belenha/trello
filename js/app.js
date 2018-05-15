eb9a4abf4f0b4edac14d1aed9ae4e0b69cf6b6de
belenha <lubinatica@gmail.com>
2018-05-15 13:02:59 -0500

    se corrigió un pequeño error en el JS

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
-var createList = document.addEventListener('click', function showMenu(event){
+var show = function (){
     var hideMenu = document.getElementsByClassName('primero')[0];
     hideMenu.style.display = 'none';
     var showMenu = document.getElementsByClassName('box')[0];
     showMenu.style.display = "block";
-});
+};
+var createList = document.getElementById('createList');
+createList.addEventListener('click', show);
