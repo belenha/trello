b794af14aa5adab2c845cdd40c828565f9b8c1ba
belenha <lubinatica@gmail.com>
2018-05-16 14:12:57 -0500

    Se sube la version 0.0.3, la pasada fue la 0.0.2, perdón!

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var inputValue = document.getElementById('input-list').value;
+// Versión 0.0.1 //
 
 var show = function (){
     var hideMenu = document.getElementsByClassName('primero')[0];
@@ -9,72 +9,44 @@ var show = function (){
 var createList = document.getElementById('createList');
 createList.addEventListener('click', show);
 
-// Versión 0.0.2 //
-
 var save = document.getElementById('greenButton');
 save.addEventListener('click', function (){
     var cardTtl = document.getElementById('input-list').value;
     // document.getElementById('input-list').value = '';
 });
 
+// Versión 0.0.2 //
+
+var cardOne = function (){
+    var createdCard = document.getElementById('newCard');    
+    var inputValue = document.getElementById('input-list').value;
+    // document.getElementById('input-list').value = '';
+    var newText = document.createTextNode(inputValue);
+    console.log(newText);
+    var titleH4 = document.getElementById('cardText');
+    titleH4.appendChild(newText);
+    var esconde = document.getElementById('hide');
+    esconde.style.display = 'none';
+    createdCard.style.display = 'block';
+};
+var showNewBox = document.getElementById('greenButton');
+showNewBox.addEventListener('click', cardOne);
+
 // Versión 0.0.3 //
 
-var firstCard = function (){
-var createdCard = document.getElementById('newCard');    
-var inputValue = document.getElementById('input-list').value;
-document.getElementById('input-list').value = '';
-var newText = document.createTextNode(inputValue);
-console.log(newText);
-var titleH4 = document.getElementById('cardText');
-titleH4.appendChild(newText);
-var esconde = document.getElementById('hide');
-esconde.style.display = 'none';
-createdCard.style.display = 'block';
-}
-var showNewBox = document.getElementById('greenButton');
-showNewBox.addEventListener('click', firstCard);
+var cardTwo = function (){
+    var secondCard = document.getElementById('secondCard')
+    var inputValueTwo = document.getElementById('input-list').value;
+    var secondCardTitle = document.createTextNode(inputValueTwo);
+    var secondTitleH4 = document.getElementById('cardTitle');
+    secondTitleH4.appendChild(secondCardTitle);
+    var write = document.getElementById('whiteArea');
+    var otherAddBtn = document.getElementById('addBtn');
+    var closeAgain = document.getElementById('secondClose');
+    var noShow = document.getElementById('newCard');
+    noShow.style.display = 'none';
+    secondCard.style.display = 'block';
+};
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // addButton.addEventListener('click', function(){
-    // var nodeText = document.createElement('p');
-    // nodeText.appendChild(newText);
-    // createdCard.appendChild(nodeText);
-
-    // var addButton = document.createElement('button');
-    // createdCard.appendChild(addButton);
-    // addButton.type = 'submit';
-    // var buttonName = document.createTextNode('Añadir una tarjeta');
-    // addButton.appendChild(buttonName);
-
-  // var firstCard = document.createElement('div');
-        // // var writeArea = document.createElement('textarea');
-        // var addGreenButton = document.createElement('button');
-        // var close = document.createElement('button');
-        // var dots = document.createElement('i');
-        // var textGreenButton = document.createTextNode('Añadir una tarjeta...');
-        // var textCloseButton = document.createTextNode('x');
-
-        // createdCard.appendChild(firstCard);
-        // firstCard.appendChild(nodeText);
-        // // firstCard.appendChild(writeArea);
-        // firstCard.appendChild(addGreenButton);
-        // firstCard.appendChild(close);
-        // firstCard.appendChild(dots);
-        // addGreenButton.appendChild(textGreenButton);
-        // close.appendChild(textCloseButton);
-
-        // firstCard.setAttribute('class', 'box');
+var task = document.getElementById('addWhiteArea');
+task.addEventListener('click', cardTwo);
